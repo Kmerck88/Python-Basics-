b70fc138e176adbb4d073187c743038dd5250ca6
Kmerck88 <kevin.merck88@gmail.com>
2021-06-15 20:34:13 -0400

    Doing basics

---
+++ main.py
@@ -0,0 +1,36 @@
+# print("Predictions are hard.")
+# print("Especially about the future")
+# user_num = 5
+# print('user_num is:', user_num)
+
+
+# FIXME: The below is wrong. Change
+#        the 1 so that the statement
+#        outputs monthly salary.
+
+
+# hourly_wage = 20
+#
+# print('Annual salary is: ')
+# print(hourly_wage * 40 * 50)
+# print()
+#
+# print('Monthly salary is: ')
+# print(((hourly_wage * 40 * 50) / 12))  # Changed 1 to 12 to get correct Monthly  salary
+# print()
+
+
+# user_num = int(input('Enter integer:\n'))
+#
+# print('You entered:', user_num)
+# print(user_num, 'squared is', user_num ** 2)
+# print('And', user_num, 'cubed is', user_num ** 3, '!!')
+#
+# user_num2 = int(input('Enter another integer:\n'))
+# print(user_num, '+', user_num2, 'is', user_num + user_num2)
+# print(user_num, '*', user_num2, 'is', user_num * user_num2)
+
+
+cell_count = int(input())
+
+print(cell_count * 10)
